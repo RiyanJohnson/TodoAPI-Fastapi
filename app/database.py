0567ea9a0847c3307f1a7b4162bf2487fa7a71c5
RiyanJohnson <john.riyan16@gmail.com>
2025-06-20 00:11:21 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,13 +2,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, MetaData
 from databases import Database
 
-DATABASE_URL = "mysql+aiomysql://root:riyan@localhost/todo"
+DATABASE_URL = "mysql+aiomysql://username:password@localhost/database_name"
 
 database = Database(DATABASE_URL)
 metadata = MetaData()
 Base = declarative_base()
 
 engine = create_engine(
-    DATABASE_URL.replace("aiomysql", "pymysql"),  # SQLAlchemy needs sync driver
+    DATABASE_URL.replace("aiomysql", "pymysql"),  
     echo=True
 )
